e5c812f8700563f9294ec55020384ced5834d980
Abisena <abisena1123@gmail.com>
2023-12-16 19:11:09 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { ProductsModule } from './products/products.module';
+import { ProductModule } from './product/product.module';
 import { UsersService } from './users/users.service';
 import { OrderModule } from './order/order.module';
-import { ProductModule } from './product/product.module';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ProductsModule, OrderModule, ProductModule],
-  controllers: [AppController, UsersModule],
+  imports: [PrismaModule, UsersModule, OrderModule, ProductModule],
+  controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
